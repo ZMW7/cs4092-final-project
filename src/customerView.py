7ef38e5ed9870b1e794bcad3141919c0549b6b41
Zachary <ZacharyScript@gmail.com>
2026-07-26 10:53:47 -0400

    Added commonTypes.py

--- src/customerView.py
+++ src/customerView.py
@@ -9,67 +9,7 @@ import textwrap
 import readline # for up arrow support on linux / mac
 import enum
 
-class ReportReason(enum.StrEnum):
-    FALSE_ADVERTISING = "false advertising",
-    INNAPROPRIATE_CONTENT = "innapropriate content",
-    CUSTOM = "custom"
-
-class AddressEntry(NamedTuple):
-    id: int
-    country: str
-    administrative_division: str | None
-    city: str
-    line1: str
-    line2: str | None
-    postal_code: str
-    customer_id: int
-
-class PaymentMethodEntry(NamedTuple):
-    id: int
-    card_number: str
-    card_expiration: datetime
-    card_code: int
-    billing_address_id: int
-    customer_id: int
-
-class PurchaseEntry(NamedTuple):
-    id: int
-    created_at: datetime
-    customer_id: int
-    payment_method_id: int
-
-class ProductSalesEntry(NamedTuple):
-    id: int
-    purchase_id: int
-    product_id: int
-    price_per_item: Decimal
-    quantity: int
-
-class DeliveryEntry(NamedTuple):
-    id: int
-    purchase_id: int
-    address_id: int
-    delivery_status: str | None
-    shipped_on: dtdate | None
-    estimated_delivery_time: datetime | None
-
-class RatingEntry(NamedTuple):
-    customer_id: int
-    product_id: int
-    rating: int
-    created_at: datetime
-
-class ReportEntry(NamedTuple):
-    id: int
-    customer_id: int
-    reviewed_by: int | None
-    reason: str
-    created_at: datetime
-
-class PurchaseHistoryEntry(NamedTuple):
-    purchase: PurchaseEntry
-    deliveries: dict[int, tuple[DeliveryEntry, str]]
-    sales: dict[int, tuple[ProductSalesEntry, str]]
+from commonTypes import AddressEntry, PaymentMethodEntry, PurchaseEntry, ProductSalesEntry, DeliveryEntry, RatingEntry, ReportEntry, PurchaseHistoryEntry
 
 def strict_truncate_str(text: str, max_length: int = 15):
     if len(text) > max_length:
